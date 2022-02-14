0390616bd6c93a505f6ba5ba40d4e514855e957f
CursedHunte <77138824+CursedHunte@users.noreply.github.com>
2022-02-14 12:36:08 +0700

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,23 +1,5 @@
-let a = 4;
-let b = -4;
-let c = 1;
-let x1, x2;
-
-// считаем дискриминант
-let D = b*b - 4*a*c;
-
-
-// смотрим знак D
-if (D > 0) {		//2 корня
-	x1 = (-b + Math.sqrt(D)) / 2*a;
-	x2 = (-b - Math.sqrt(D)) / 2*a;
-	console.log(`x1 = ${x1}`)
-	console.log(`x1 = ${x2}`)
-}
-else if (D == 0) {		//1 корень
-	x1 = -b / 2*a;
-	console.log(`x1 = ${x1}`)
-}
-else { //корней нет
-	console.log('действительных корней нет')
-}
+let height = 23;
+let width = 10;
+let s;
+s = 1/2 * width * height;
+console.log(`s=${s}`)
